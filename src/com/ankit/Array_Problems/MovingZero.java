137789136074814601d0994717fb874643665352
Ankitkansal2510 <kansal920@gmail.com>
2019-03-22 06:53:02 -0700

    Move zero to end

---
+++ src/com/ankit/Array_Problems/MovingZero.java
@@ -0,0 +1,34 @@
+package com.ankit.Array_Problems;
+
+import java.util.Arrays;
+
+public class MovingZero {
+
+	public static void main(String[] args) {
+		int arr[]={0,1,0,3,12};
+		moveZeros(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	private static void moveZeros(int[] arr) {
+		int n=arr.length;
+		int count = 0;  // Count of non-zero elements 
+		  
+        // Traverse the array. If element encountered is 
+        // non-zero, then replace the element at index 'count' 
+        // with this element 
+        for (int i = 0; i < n; i++) 
+        {
+            if (arr[i] != 0) 
+                arr[count++] = arr[i]; // here count is 
+        }            // incremented 
+  
+        // Now all non-zero elements have been shifted to 
+        // front and 'count' is set as index of first 0. 
+        // Make all elements 0 from count to end. 
+        while (count < n) 
+            arr[count++] = 0; 
+
+}
+}
